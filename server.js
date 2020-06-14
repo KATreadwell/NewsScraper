13b4b94d3c2715911e58d59ee656ea83fe008894
KAT <katreadwell1@gmail.com>
2020-06-13 22:56:35 -0700

    vanquished awful handlebars security vulnerability that prevented display of link, yay!

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
+const _handlebars = require('handlebars');
+const handlebars = require('express-handlebars');
+const {allowInsecurePrototypeAccess}=require('@handlebars/allow-prototype-access');
 require("dotenv").config()
 
 var MONGODB_URI = process.env.MONGODB_URI
@@ -20,7 +23,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+app.set('view engine','handlebars');
+app.engine('handlebars', handlebars({
+  layoutDirs: __dirname+'/views/layouts',
+  handlebars: allowInsecurePrototypeAccess(_handlebars)
+}));
 
 app.get("/scrape", function(req, res) {
   axios.get("https://www.nytimes.com/section/opinion/technology").then(function(response) {
@@ -59,7 +66,6 @@ app.get("/", function(req, res){
     .then(function(dbArticles){
         res.render("index", {
             dbArticles
-
         }) 
     })
     .catch(function(err){
@@ -67,9 +73,22 @@ app.get("/", function(req, res){
     })
 }) 
 
-
 app.get("/articles", function(req, res) {
-  db.Article.find({})
+    db.Article.find({})
+      .then(function(dbArticles){
+        
+        res.render("index", {
+          Articles: dbArticles,
+        }) 
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+
+app.get("/savedArticles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
     .then(function(dbArticle) {
       res.json(dbArticle);
     })
@@ -78,41 +97,21 @@ app.get("/articles", function(req, res) {
     });
 });
 
-// // Route for grabbing a specific Article by id, populate it with saved?
-// app.get("/articles/:id", function(req, res) {
-//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//   db.Article.findOne({ _id: req.params.id })
-//     // ..and populate all of the notes associated with it
-//     .populate("note")
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find an Article with the given id, send it back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
-
-// // Route for saving/updating an Article's associated Note
-// app.post("/articles/:id", function(req, res) {
-//   // Create a new note and pass the req.body to the entry
-//   db.Note.create(req.body)
-//     .then(function(dbNote) {
-//       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-//       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-//     })
-//     .then(function(dbArticle) {
-//       // If we were able to successfully update an Article, send it back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+// Route for saving/unsaving an Article
+app.post("/articles/:id", function(req, res) {
+  db.Article.update(req.body)
+    .then(function(saveArticle) {
+      return db.Article.update({ _id: req.params.id }, { saved: true});
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 app.listen(PORT, function() {
   console.log("App running on http://localhost:" + PORT);
