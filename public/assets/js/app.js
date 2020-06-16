e6681373a73e2a04dbf384f889d1a511ed2e53f3
KAT <katreadwell1@gmail.com>
2020-06-15 18:43:45 -0700

    clear/scrape works, need to add functionality to save

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,71 +1,66 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    // Scrape Articles
+    $(document).on("click", "#scrape", function () {
+        $.ajax({
+            method: "GET",
+            url: "/scrape",
+            success: function (data) {
+                if (data) {
+                    if (data) {
+                        Swal.fire({
+                            title: 'Good job!',
+                            text: 'Articles Scrapped Successfully!',
+                            icon: 'success'
+                        }).then((result) => {
+                            location.reload();
+                        })
+                    }
 
-
-
-
-// Scrape New Articles
-$(document).on("click", "#scrape", function () {
-    $.ajax({
-        method: "GET",
-        url: "/scrape",
-        success: function(){
-            if(data){
-                Swal.fire(
-                    'Good job!',
-                    'Articles Scrapped Successfully!',
-                    'success'
-                  )
+                }
             }
-          
-              
-
-        }
+        })
     })
-})
 
 
-// $.getJSON("/", function (data) {
-//     for (let i = 0; i < data.length; i++) {
-//         $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>" + data[i].summary);
-//     }
-// });
+    // To Save Article
+    $(document).on("click", "#saveArticle", function () {
+        let thisId = $(this).attr("data-id");
 
+        $.ajax({
+            method: "POST",
+            url: "/articles/" + thisId,
+        })
+    });
 
-// To Save Article
-$(document).on("click", "#saveArticle", function () {
-    let thisId = $(this).attr("data-id");
+    // Saved Articles
+    $(document).on("click", "#savedArticles", function () {
 
-    $.ajax({
-        method: "POST",
-        url: "/articles/" + thisId,
-    })
-});
+        $.ajax({
+            method: "GET",
+            url: "/savedArticles"
+        })
+    });
 
-// Saved Articles
-$(document).on("click", "#savedArticles", function () {
+    // Clear Articles
+    $(document).on("click", "#clear", function () {
 
-    $.ajax({
-        method: "GET",
-        url: "/savedArticles"
-    })
-});
+        $.ajax({
+            method: "GET",
+            url: "/clearArticles",
+            success: function (data) {
+                if (data) {
+                    if (data) {
+                        Swal.fire({
+                            title: 'Good job!',
+                            text: 'Articles Deleted Successfully!',
+                            icon: 'success'
+                        }).then((result) => {
+                            location.reload();
+                        })
+                    }
 
-// Clear Articles
-$(document).on("click", "#clear", function () {
-
-    $.ajax({
-        method: "GET",
-        url: "/clearArticles",
-        success: function(data){
-            if(data){
-                Swal.fire(
-                    'Good job!',
-                    'Articles Deleted Successfully!',
-                    'success'
-                  )
+                }
             }
-         
-        }
-    })
-});
+        })
+    });
 })
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cheerio = require("cheerio");
 const db = require("./models");
 const _handlebars = require("handlebars");
 const handlebars = require("express-handlebars");
-const {allowInsecurePrototypeAccess} = require("@handlebars/allow-prototype-access");
+const { allowInsecurePrototypeAccess } = require("@handlebars/allow-prototype-access");
 
 const MONGODB_URI = process.env.MONGODB_URI
 
@@ -25,32 +25,19 @@ app.use(express.json());
 app.use(express.static(__dirname + "public"));
 app.use(express.static("."))
 
-app.set("view engine","handlebars");
+app.set("view engine", "handlebars");
 app.engine("handlebars", handlebars({
-  layoutDirs: __dirname+"/views/layouts",
+  layoutDirs: __dirname + "/views/layouts",
   handlebars: allowInsecurePrototypeAccess(_handlebars)
 }));
 
-//Homepage 
-// app.get("/", function(req, res){
-//   console.log('its working')
-//   db.Article.find({})
-//   .then(function(dbArticle) {
-//     // If we were able to successfully find Articles, send them back to the client
-//     res.json(dbArticle);
-//   })
-//   .catch(function(err){
-//       res.json(err);
-//   })
-// }) 
-
 //Scrape New Articles
-app.get("/scrape", function(req, res) {
-  axios.get("https://www.nytimes.com/section/opinion/technology").then(function(response) {
+app.get("/scrape", function (req, res) {
+  axios.get("https://www.nytimes.com/section/opinion/technology").then(function (response) {
     let $ = cheerio.load(response.data);
     let articleArray = [];
 
-    $(".css-ye6x8s").each(function(i, element) {
+    $(".css-ye6x8s").each(function (i, element) {
       let result = {};
       result.title = $(this)
         .find("h2")
@@ -58,96 +45,82 @@ app.get("/scrape", function(req, res) {
       result.link = "https://www.nytimes.com" + $(this)
         .find("a")
         .attr("href");
-      result.summary = $(this,"p")
+      result.summary = $(this, "p")
         .first()
         .text();
 
-        articleArray.push(result)
+      articleArray.push(result)
     });
-   
-    db.Article.create(articleArray)
-    .then(function(dbArticle) {
-      console.log(dbArticle);
-      res.send("Scrape Complete");
-    })
 
-    .catch(function(err) {
-      console.log(err);
-    });   
-  }); 
+    db.Article.create(articleArray)
+      .then(function (dbArticle) {
+        console.log(dbArticle);
+        res.send("Scrape Complete");
+      })
+
+      .catch(function (err) {
+        console.log(err);
+      });
+  });
 });
 
 
-// All articles scrapped
-app.get("/", function(req, res) {
-    db.Article.find({})
-      .then(function(dbArticles){
-        
-        res.render("index", {
-          Articles: dbArticles,
-        }) 
+//Homepage
+app.get("/", function (req, res) {
+  db.Article.find({})
+    .then(function (dbArticles) {
+
+      res.render("index", {
+        Articles: dbArticles,
+      })
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
-// All articles scrapped
-app.get("/clearArticles", function(req, res) {
+//Clear Articles
+app.get("/clearArticles", function (req, res) {
   db.Article.deleteMany({})
-    .then(function(dbArticles){
-      
+    .then(function (dbArticles) {
+
       res.render("index", {
         Articles: dbArticles,
-      }) 
-  })
-  .catch(function(err) {
-    res.json(err);
-  });
+      })
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
 
 //Saved Articles
-app.get("/savedArticles", function(req, res) {
-  db.Article.findOne({ _id: req.params.id,  saved: true })
-    .then(function(dbArticle) {
+app.get("/savedArticles", function (req, res) {
+  db.Article.findOne({ _id: req.params.id, saved: true })
+    .then(function (dbArticle) {
       res.render("partials/saved-articles", {
         Articles: dbArticle,
-      }) 
+      })
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
 //to save articles
-app.post("/articles/:id", function(req, res) {
-  db.Article.update(req.body)
-    .then(function(saveArticle) {
-      return db.Article.update({ _id: req.params.id }, { saved: true});
-    })
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-//to clear articles
-app.post("/articles/:id", function(req, res) {
-  db.Article.update(req.body)
-    .then(function(saveArticle) {
-      return db.Article.update({ _id: req.params.id }, { saved: true});
-    })
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+// app.post("/articles/:id", function(req, res) {
+//   db.Article.update(req.body)
+//     .then(function(saveArticle) {
+//       return db.Article.update({ _id: req.params.id }, { saved: true});
+//     })
+//     .then(function(dbArticle) {
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+// });
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on http://localhost:" + PORT);
 });
